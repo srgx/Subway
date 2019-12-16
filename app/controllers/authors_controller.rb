ecf523f9570942f320e1c1e39426e45dd3328919
Jan Kowalski <yakov85@o2.pl>
2019-12-16 12:06:03 +0100

    Create and destroy authors

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,4 +7,29 @@ class AuthorsController < ApplicationController
     @author=Author.find(params[:id])
     @books=@author.books
   end
+
+  def new
+    @author = Author.new
+  end
+
+  def create
+    # render plain: params[:author].inspect
+    @author=Author.new(author_params)
+    if @author.save
+      redirect_to authors_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @author=Author.find(params[:id])
+    @author.destroy
+    redirect_to authors_path
+  end
+
+  private
+    def author_params
+      params.require(:author).permit(:name)
+    end
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,4 @@
 class Author < ApplicationRecord
-  has_many :books
+  has_many :books, dependent: :destroy
   validates :name, presence: true
 end
