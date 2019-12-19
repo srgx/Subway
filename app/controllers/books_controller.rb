6e56ae8f526ca75b4094c7e98b041464485eeb7e
Jan Kowalski <yakov85@o2.pl>
2019-12-19 23:53:30 +0100

    Create and destroy books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,31 @@
 class BooksController < ApplicationController
   def index
-    @books=Book.all
+    @author=Author.find(params[:author_id])
+    @books=Book.where(author:@author)
   end
 
   def show
     @book=Book.find(params[:id])
   end
+
+  def new
+    @author=Author.find(params[:author_id])
+  end
+
+  def create
+    @author=Author.find(params[:author_id])
+    @book=@author.books.create(book_params)
+    redirect_to author_books_path(@author)
+  end
+
+  def destroy
+    @book=Book.find(params[:id])
+    @book.destroy
+    redirect_to author_books_path
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :description)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'authors#index'
   resources :books
-  resources :authors
+  resources :authors do
+    resources :books
+  end
 end
